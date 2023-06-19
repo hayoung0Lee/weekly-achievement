a64d7ce41df4788820c85b08abbf964589818202
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2023-06-19 23:14:30 +0900

    Time: 7 ms (24.99%), Space: 7.5 MB (89.69%) - LeetHub

--- Leetcode/0063-unique-paths-ii/hayoung0Lee_0063-unique-paths-ii.cpp
+++ Leetcode/0063-unique-paths-ii/hayoung0Lee_0063-unique-paths-ii.cpp
@@ -26,12 +26,12 @@ public:
             }
         }
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                std::cout << arr[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         std::cout << arr[i][j] << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
         
         return arr[m-1][n-1];
     }
