4cf6cd962e470868cbbd3bd6a3020f239b0d2a13
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2023-06-24 14:56:21 +0900

    Time: 2384 ms (5.01%), Space: 9.1 MB (67.59%) - LeetHub

--- Leetcode/0047-permutations-ii/hayoung0Lee_0047-permutations-ii.cpp
+++ Leetcode/0047-permutations-ii/hayoung0Lee_0047-permutations-ii.cpp
@@ -28,6 +28,7 @@ public:
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size(); i++){
             arr[nums[i] + 10]++;
         }
