373f7e65eddf8fa4286b79e0784cee248393e69d
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2023-09-24 13:24:35 +0900

    Time: 71 ms (71.61%), Space: 48.3 MB (42.17%) - LeetHub

--- Leetcode/0888-fair-candy-swap/hayoung0Lee_0888-fair-candy-swap.cpp
+++ Leetcode/0888-fair-candy-swap/hayoung0Lee_0888-fair-candy-swap.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-        vector<int> ans(2, -1); // {-1, -1}
-        map <int,int> mp; 
+        vector<int>ans;
+        int asum=0,bsum=0;
+        for(auto x:aliceSizes) 
+            asum+=x;
+        for(auto x:bobSizes)
+            bsum+=x;
+        int diff=(bsum-asum)/2; // bobSizes: 4, AliceSizes: 2 -> diff: 1 각각 1을 빼고 1을 더하면 같아짐
+        unordered_set<int> s(bobSizes.begin(), bobSizes.end());
         
-        int aliceSum = 0; 
-        for(int i = 0; i < aliceSizes.size(); i++){
-            aliceSum += aliceSizes[i];
-        }
-        
-        int bobSum = 0; 
-        for(int i = 0; i < bobSizes.size(); i++){
-            mp[bobSizes[i]] = 1; // add value
-            bobSum += bobSizes[i];
-        }
-        
-        // aliceSum - a[i] + b[j] = bobSum - b[j] + a[i]
-        // b[i] = (bobSum - aliceSum + a[i] + a[i]) / 2
-        for(int i = 0; i < aliceSizes.size(); i++){
-            if(mp.find((bobSum - aliceSum + aliceSizes[i] + aliceSizes[i]) / 2) != mp.end()){
-                ans[0] = aliceSizes[i];
-                ans[1] = (bobSum - aliceSum + aliceSizes[i] + aliceSizes[i]) / 2;
+        for(auto x: aliceSizes)
+        {
+            // 1 + 1 => 2 2가 bobSizes에 있다는 뜻은 1(Alice)이랑 2(Bob)랑 바꾼다는 뜻인데, 
+            // [1, 1] [1 + diff, 2] 를 바꾼다는 뜻
+            if(s.find(x + diff) != s.end()){
+                ans.push_back(x);
+                ans.push_back(x+diff);
+                break;
             }
-        }
-
+        }      
+        
         return ans;
     }
 };
