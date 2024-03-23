e390a79f1b2e0147f3822cc9b207292444a735ef
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2024-03-23 19:40:04 +0900

    Time: 5 ms (27.55%), Space: 11 MB (72.95%) - LeetHub

--- Leetcode/0088-merge-sorted-array/hayoung0Lee_0088-merge-sorted-array.cpp
+++ Leetcode/0088-merge-sorted-array/hayoung0Lee_0088-merge-sorted-array.cpp
@@ -14,18 +14,18 @@ public:
         while(i < s && j < n){ //  
             if(nums1[i] >= nums2[j]){
                 for(int k = s; k > i; k--){
-                   nums1[k] = nums1[k-1]; 
+                   nums1[k] = nums1[k-1];  // 새로운 원소가 추가되면 뒤로 민다
                 }
                 nums1[i] = nums2[j];
-                s++;  
-                j++;
+                s++;  // 그리고 빈자리를 하나 줄인다, 한개가 추가되었으니 1을 증가시킴
+                j++; // 그리고 그다음 원소 체크를 위해 1을 증가시킴
             }
-            i++;
+            i++; // 더 크던, 작던 같던 nums1[i]는 검사를 마쳤으므로 증가시킨다. 
         }
         
         if(j < n){
            for(int k = i; k < nums1.size(); k++){
-                nums1[k] = nums2[j];
+                nums1[k] = nums2[j]; // 나머지 원소를 옮겨준다
                 j++;
             } 
         }
