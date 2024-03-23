ba75c0b2ccedbd99de310fc3a8d810ab968fd84e
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2024-03-23 18:28:23 +0900

    Time: 5 ms (27.54%), Space: 10.3 MB (80.43%) - LeetHub

--- Leetcode/0027-remove-element/hayoung0Lee_0027-remove-element.cpp
+++ Leetcode/0027-remove-element/hayoung0Lee_0027-remove-element.cpp
@@ -3,13 +3,17 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int i = 0;
         int n = nums.size();
+    
         while(i < n){
+            // 같으면 위치를 다 옮겨준다, 그리고 길이를 1만큼 줄여서 길이를 줄인다
             if(nums[i] == val){
                 for(int j = i; j < nums.size() - 1; j++){
                     nums[j] = nums[j + 1];
                 }
+                // 이때는 i값을 올리지 않는다, 왜냐하면 중복된 값이 있을수도 있기 때문에
                 n--;
             } else {
+                // 값이다르면 그냥 다음값체크하러간다
                 i++;
             }
         }
