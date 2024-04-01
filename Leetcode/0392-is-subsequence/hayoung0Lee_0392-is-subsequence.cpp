d9d82be2ffeb1b5f6e0ee9199b808e7d662d93af
Hayoung Lee(이하영) <70381940+hayoung0Lee@users.noreply.github.com>
2024-04-01 22:16:04 +0900

    Time: 3 ms (47.45%), Space: 7.6 MB (32.17%) - LeetHub

---
+++ Leetcode/0392-is-subsequence/hayoung0Lee_0392-is-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int left = 0;
+        int right = 0;
+        
+        while(left <= s.size() && right <= t.size()){
+            if(s[left] == t[right]){
+                left++;
+                right++;
+            }else{
+                right++;
+            }
+        }
+        
+        if(left < s.size()){
+            return false;
+        }
+        
+        return true;
+    }
+};
